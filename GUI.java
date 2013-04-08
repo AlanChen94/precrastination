a0ab75a69eafc079e5b2cb5d3f1fa90e1eb56b05
MyNameIsDhaval <dhavalp.00@gmail.com>
2013-04-08 16:02:29 -0400

    Added GUI file

---
+++ GUI.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class GUI{
+
+	private static class MainDisplay extends JPanel {
+		public void paintComponents(Graphics g);
+		
+	}
+
+}
